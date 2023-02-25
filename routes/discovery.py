d475a714b4dada78e9df087e31ed940dc5c3104f
KotaHemanthUC <114269470+KotaHemanthUC@users.noreply.github.com>
2023-02-25 13:22:19 -0800

    add file view
    Added route to see files of other peer

--- routes/discovery.py
+++ routes/discovery.py
@@ -1,7 +1,20 @@
 from flask import Blueprint
+import os
 
 simple_page = Blueprint('simple_page', __name__, template_folder='templates')
 
 @simple_page.get('/discover')
 def get_known_node_list():
     return "<p>Hello, World!</p>"
+
+@simple_page.get('/view_files')
+def get_files():
+    directory_tree = {}
+    key = "/Users/hemanthkota/Downloads/"
+    for i in os.listdir('.'):
+       if os.path.iffile(i):
+          if key in directory_tree:
+             directory_tree[key].append(i)
+          else:
+             directory_tree[key] = [i]
+    return directory_tree
